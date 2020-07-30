463e3cf6db5b7a88470a80391facfb9d1cd68c3c
tyoeer <tyoeer@users.noreply.github.com>
2020-07-30 16:08:30 +0200

    Adder error for invalid initialization

--- init.lua
+++ init.lua
@@ -7,9 +7,12 @@ local f = function(mc)
 		middleclass = mc
 		require(path.."oop")
 		middleclass = oldMiddleclass
+		print("Done setup!")
 		return f
 	elseif type(mc)=="string" then
 		return require(path)
+	else
+		error("Invalid input: "..type(mc))
 	end
 end
 
