4c3f33e898f8ef18aafb88916169317337a4c445
tyoeer <tyoeer@users.noreply.github.com>
2020-07-30 16:12:20 +0200

    Potential fix for initialization not properly working

--- init.lua
+++ init.lua
@@ -1,13 +1,12 @@
 local path = select(1,...) .. "."
-
-local f = function(mc)
+local f
+f = function(mc)
 	if type(mc)=="function" or type(mc)=="table" then
 		--setup OOP
 		local oldMiddleclass = middleclass
 		middleclass = mc
 		require(path.."oop")
 		middleclass = oldMiddleclass
-		print("Done setup!")
 		return f
 	elseif type(mc)=="string" then
 		return require(path)
